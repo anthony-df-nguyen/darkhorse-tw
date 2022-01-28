03f0a217ccaed393fabb52aaa22a6c3a8df1f2ac
skifreetony <skifreetony@yahoo.com>
2022-01-27 23:31:23 -0800

    placeholder temporary recommendation component

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import Link from "next/link";
 import Page from "../components/page";
 import Header from "../components/header";
 import InfoCard from "../components/info-card";
+import ImagePanel from "../components/ImagePanel";
+import Recommendation from "../components/Recommendation";
 import { ExternalLinkIcon } from "@heroicons/react/solid";
 
 export default function Home() {
@@ -26,117 +28,26 @@ export default function Home() {
           </a>
         </div> */}
       </Header>
-      <div className="relative py-16 bg-white">
-        <div
-          className="hidden absolute top-0 inset-x-0 h-1/2 bg-gray-50 lg:block"
-          aria-hidden="true"
-        />
-        <div className="max-w-7xl mx-auto bg-indigo-600 lg:bg-transparent lg:px-8">
-          <div className="lg:grid lg:grid-cols-12">
-            <div className="relative z-10 lg:col-start-1 lg:row-start-1 lg:col-span-4 lg:py-16 lg:bg-transparent">
-              <div
-                className="absolute inset-x-0 h-1/2 bg-gray-50 lg:hidden"
-                aria-hidden="true"
-              />
-              <div className="max-w-md mx-auto px-4 sm:max-w-3xl sm:px-6 lg:max-w-none lg:p-0">
-                <div className="aspect-w-10 aspect-h-6 sm:aspect-w-2 sm:aspect-h-1 lg:aspect-w-1">
-                  <img
-                    className="object-cover object-center rounded-3xl shadow-2xl"
-                    src="/images/services2.jpg"
-                    alt=""
-                  />
-                </div>
-              </div>
-            </div>
+      <ImagePanel />
+      <Recommendation
+        name="Carlos and Mercedes Carchi"
+        title="Sr. Network Manager, Belkin International"
+        side="left">
+        {`"Over the past 13 years, Alan has provided us a framework of a thorough planning process and ongoing quarterly portfolio reviews that has helped us make informed decisions about our money, and with a steady hand, guided us through the good times and bad ones"`}
+      </Recommendation>
 
-            <div className="relative bg-accent lg:col-start-3 lg:row-start-1 lg:col-span-10 lg:rounded-3xl lg:grid lg:grid-cols-10 lg:items-center">
-              <div
-                className="hidden absolute inset-0 overflow-hidden rounded-3xl lg:block"
-                aria-hidden="true">
-                <svg
-                  className="absolute bottom-full left-full transform translate-y-1/3 -translate-x-2/3 xl:bottom-auto xl:top-0 xl:translate-y-0"
-                  width={404}
-                  height={384}
-                  fill="none"
-                  viewBox="0 0 404 384"
-                  aria-hidden="true">
-                  <defs>
-                    <pattern
-                      id="64e643ad-2176-4f86-b3d7-f2c5da3b6a6d"
-                      x={0}
-                      y={0}
-                      width={20}
-                      height={20}
-                      patternUnits="userSpaceOnUse">
-                      <rect
-                        x={0}
-                        y={0}
-                        width={4}
-                        height={4}
-                        className="text-emerald-800"
-                        fill="currentColor"
-                      />
-                    </pattern>
-                  </defs>
-                  <rect
-                    width={404}
-                    height={384}
-                    fill="url(#64e643ad-2176-4f86-b3d7-f2c5da3b6a6d)"
-                  />
-                </svg>
-                <svg
-                  className="absolute top-full transform -translate-y-1/3 -translate-x-1/3 xl:-translate-y-1/2"
-                  width={404}
-                  height={384}
-                  fill="none"
-                  viewBox="0 0 404 384"
-                  aria-hidden="true">
-                  <defs>
-                    <pattern
-                      id="64e643ad-2176-4f86-b3d7-f2c5da3b6a6d"
-                      x={0}
-                      y={0}
-                      width={20}
-                      height={20}
-                      patternUnits="userSpaceOnUse">
-                      <rect
-                        x={0}
-                        y={0}
-                        width={4}
-                        height={4}
-                        className="text-indigo-500"
-                        fill="currentColor"
-                      />
-                    </pattern>
-                  </defs>
-                  <rect
-                    width={404}
-                    height={384}
-                    fill="url(#64e643ad-2176-4f86-b3d7-f2c5da3b6a6d)"
-                  />
-                </svg>
-              </div>
-              <div className="relative max-w-md mx-auto py-12 px-4 space-y-6 sm:max-w-3xl sm:py-16 sm:px-6 lg:max-w-none lg:p-0 lg:col-start-4 lg:col-span-6">
-                <h2
-                  className="text-3xl font-extrabold text-white"
-                  id="join-heading">
-                  Services
-                </h2>
-                <p className="text-lg text-gray-50">
-                  Our wealth management services which include a broad range of
-                  comprehensive financial planning and consulting services
-                  management of investment portfolios.
-                </p>
-                <a
-                  className="block w-full py-3 px-5 text-center bg-gray-100 border border-transparent rounded-md shadow-md text-base font-medium text-dark hover:bg-gray-50 sm:inline-block sm:w-auto"
-                  href="#">
-                  View Services
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Recommendation
+        name="Dr. Brad and Kathy de Marquette"
+        title="Emergency Physician, Kaiser Permanante"
+        side="right">
+        {`"Alan has carefully listened and has tailor-made a thoughtful financial plan that matches our comfort level and our long-term wishes for our family. We are confident with his asset management process that allows us the freedom to focus on the other things"`}
+      </Recommendation>
+      <Recommendation
+        name="Dr. Janet K. Ruffing, RSM"
+        title="Yale University"
+        side="left">
+        {`"You are a true scholar in your research efforts and your personal investment guidance"`}
+      </Recommendation>
     </Page>
   );
 }
