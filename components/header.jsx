d6b661ec6a31d447e77bb62486f42daf04ac0774
Ken Kieu <kennyykieu@gmail.com>
2022-01-27 17:33:28 -0800

    Added icons

--- components/header.jsx
+++ components/header.jsx
@@ -1,7 +1,7 @@
 export default function Header(props) {
   return (
     <div className="bg-gray-100">
-      <div className="max-w-7xl mx-auto pad-tb px-4 sm:py-24 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto px-4 pad-tb sm:px-6 lg:px-8">
         <div className="text-center">
           <h2 className="font-semibold text-accent tracking-wide uppercase text-sm lg:text-base">
             {props.subtitle}
@@ -9,7 +9,7 @@ export default function Header(props) {
           <p className="mt-1 text-2xl font-extrabold text-dark sm:text-2xl sm:tracking-tight lg:text-4xl">
             {props.title}
           </p>
-          <p className="max-w-3xl mt-5 mx-auto text-xl text-standard">
+          <p className="max-w-3xl mt-5 mx-auto text-base lg:text-lg text-gray">
             {props.children}
           </p>
         </div>
--- components/info-card.jsx
+++ components/info-card.jsx
@@ -5,6 +5,7 @@ import {
 } from "@heroicons/react/outline";
 
 import { GiMagnifyingGlass } from "react-icons/gi";
+import { FaHandshake, FaBriefcase } from "react-icons/fa";
 
 const features = [
   {
@@ -17,42 +18,50 @@ const features = [
     name: "Service",
     description:
       "DHA is committed to understanding long and short-term financial and investment objectives, needs, and risk tolerances of each customer. Aligning a well-built investment portfolio for each unique client profile substantially increases the likelihood of achieving positive wealth goals.",
-    icon: ScaleIcon,
+    icon: FaBriefcase,
   },
   {
     name: "Integrity",
     description:
       "DHA serves its clients’ needs first and foremost by acting as an independent fiduciary advisor. DHA has no affiliation with any products or service companies including wholesalers or retail brokerages. Because of this, DHA has no conflict of interest and does not receive any commission fees which assures DHA advice will always be centered on what is best for the client. All client fee arrangements are modest and transparent. The fee is based on the value of the account(s) under management.",
-    icon: LightningBoltIcon,
+    icon: FaHandshake,
   },
 ];
-export default function InfoCard() {
+export default function Example() {
   return (
-    <div className="relative bg-white pad-top">
-      <div className="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
-        <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
-          {features.map((feature) => (
-            <div key={feature.name} className="pt-6">
-              <div className="flow-root bg-gray-50 rounded-lg px-6 pb-8">
-                <div className="-mt-6">
-                  <div>
-                    <span className="inline-flex items-center justify-center p-3 bg-sky-600 rounded-md shadow-lg">
-                      <feature.icon
-                        className="h-6 w-6 text-white"
-                        aria-hidden="true"
-                      />
-                    </span>
+    <div className="pad-tb bg-white">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        {/* <div className="lg:text-center">
+          <h2 className="text-base text-accent font-semibold tracking-wide uppercase">
+            Transactions
+          </h2>
+          <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            A better way to send money
+          </p>
+          <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+            Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam
+            voluptatum cupiditate veritatis in accusamus quisquam.
+          </p>
+        </div> */}
+
+        <div className="mt-10">
+          <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-1 md:gap-x-8 md:gap-y-10">
+            {features.map((feature) => (
+              <div key={feature.name} className="relative">
+                <dt>
+                  <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-sky-500 text-white">
+                    <feature.icon className="h-6 w-6" aria-hidden="true" />
                   </div>
-                  <h3 className="mt-8 text-lg font-medium text-gray-900 tracking-tight">
+                  <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
                     {feature.name}
-                  </h3>
-                  <p className="mt-5 text-base leading-8 text-gray-500">
-                    {feature.description}
                   </p>
-                </div>
+                </dt>
+                <dd className="mt-2 ml-16 text-base text-gray-500">
+                  {feature.description}
+                </dd>
               </div>
-            </div>
-          ))}
+            ))}
+          </dl>
         </div>
       </div>
     </div>
